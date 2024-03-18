c300ddb628ef2bcb05dd1697c1036c6927055297
Lucy Ruiz <lucyruizwa@gmail.com>
2024-03-18 14:34:01 -0700

    convert string into integer to acces index and assign value

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ let currentlyPlaying;
 const startBtn = document.getElementById("startBtn");
 const playAgainBtn = document.getElementById("playAgainBtn");
 
-
 // --- event listeners
 startBtn.addEventListener("click", startGame);
 
@@ -24,16 +23,23 @@ init()
 function init() {
     timer = 60;
     points = 0;
+    grid = [null, null, null, null, null, null, null, null, null, null];
     currentlyPlaying = false;
+
     render()
 }
 
 function startGame(evt) {
     evt.preventDefault()
-    if(evt.target.tagName !== "DIV") {
+    if(evt.target.tagName !== "DIV" && evt.target.tagName !== "BUTTON") {
         return;
     }
     let i = parseInt(evt.target.id, 10)
+    if (grid[i] !== null) {
+        return
+    } else {
+        grid[i] = Math.floor(Math.random() * grid.length);
+    }
     if (currentlyPlaying === true) {
         return;
     }
@@ -50,18 +56,39 @@ function remainingTime() {
     renderRemainingTime()
 }
 
+function countPoints() {
 
+}
 
 function renderPoints() {};
+    const pointsEl = document.getElementById("points-display");
+    pointsEl.innerText = points;
 
 function renderRemainingTime() {
-    const timerEl = document.getElementById("time-display")
-    timerEl.innerText = timer
+    const timerEl = document.getElementById("time-display");
+    timerEl.innerText = timer;
 };
 
+function renderGrid () {
+    grid.forEach(function(gridValue, gridIdx){
+        const spaceshipEl = document.getElementById(gridIdx)
+        spaceshipEl.innerText = gridValue;
+    })
+        
+    
+};
 
 function render() {
     renderRemainingTime();
     renderPoints();
-
+    renderGrid()
 }
+
+/*
+function renderControls() {
+    playAgainBtn.style.visibility = winner ? "visible" : "hidden";
+    markerEls.forEach(function(markerEl, idx) {
+        const hideMarker = !board[idx].includes(null) || winner
+        markerEl.style.visibility = hideMarker ? "hidden" : "visible"
+    });
+} */
